8333e390b3598a46822b6758093ad8eeb2a94cbb
jeffrey-arias <jeffrey.e.arias@gmail.com>
2023-02-15 06:38:31 +0800

    Added fake API implementation and test cases.

--- src/__tests__/utils/api.test.ts
+++ src/__tests__/utils/api.test.ts
@@ -1,7 +1,28 @@
-import { apiCall } from "../../utils/api";
+import { LoginResultType } from "../../types";
+import { loginApiCall } from "../../utils/api";
 
 describe('Call API', () => {
-    it('Should run', () => {
-        // implementation here
+    const un: string = "username";
+    const pw: string = "password";
+    const resultSuccess: LoginResultType = {
+        result: "SUCCESS"
+    }
+    const resultFailed: LoginResultType = {
+        result: "FAILED"
+    }
+
+    it('Should run', async () => {
+        const result =  await loginApiCall(un, pw);
+        expect(result).toHaveProperty("result");
+    });
+
+    it('Should return SUCCESS', async () => {
+        const result =  await loginApiCall(un, pw, true);
+        expect(result).toMatchObject(resultSuccess);
+    });
+
+    it('Should return FAILED', async () => {
+        const result =  await loginApiCall(un, pw, false);
+        expect(result).toMatchObject(resultFailed);
     });
 })
